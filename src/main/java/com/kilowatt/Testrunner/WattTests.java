b980c3eadb754838e9d1d846c704473e29e597a2
Slavlned <slavlendstudio@gmail.com>
2025-04-06 12:43:24 +0500

    tests upd

--- src/main/java/com/kilowatt/Testrunner/WattTests.java
+++ src/main/java/com/kilowatt/Testrunner/WattTests.java
@@ -7,55 +7,55 @@ public class WattTests {
     public static void run() {
         WattTestRunner runner = new WattTestRunner(
                 WattTest.builder().path(
-                                "E:\\wat_lang\\wat\\src\\test\\watt\\tests\\main\\logical.wt")
+                                "G:\\Kilowatt\\Watt\\src\\test\\watt\\tests\\main\\logical.wt")
                         .name("logical")
                         .build(),
                 WattTest.builder().path(
-                                "E:\\wat_lang\\wat\\src\\test\\watt\\tests\\main\\match.wt")
+                                "G:\\Kilowatt\\Watt\\src\\test\\watt\\tests\\main\\match.wt")
                         .name("pattern matching")
                         .build(),
                 WattTest.builder().path(
-                                "E:\\wat_lang\\wat\\src\\test\\watt\\tests\\main\\types.wt")
+                                "G:\\Kilowatt\\Watt\\src\\test\\watt\\tests\\main\\types.wt")
                         .name("custom types")
                         .build(),
                 WattTest.builder().path(
-                                "E:\\wat_lang\\wat\\src\\test\\watt\\tests\\main\\units.wt")
+                                "G:\\Kilowatt\\Watt\\src\\test\\watt\\tests\\main\\units.wt")
                         .name("custom units")
                         .build(),
                 WattTest.builder().path(
-                                "E:\\wat_lang\\wat\\src\\test\\watt\\tests\\main\\while.wt")
+                                "G:\\Kilowatt\\Watt\\src\\test\\watt\\tests\\main\\while.wt")
                         .name("while")
                         .build(),
                 WattTest.builder().path(
-                                "E:\\wat_lang\\wat\\src\\test\\watt\\tests\\main\\for.wt")
+                                "G:\\Kilowatt\\Watt\\src\\test\\watt\\tests\\main\\for.wt")
                         .name("for")
                         .build(),
                 WattTest.builder().path(
-                                "E:\\wat_lang\\wat\\src\\test\\watt\\tests\\fp\\anonymous_fn.wt").
+                                "G:\\Kilowatt\\Watt\\src\\test\\watt\\tests\\fp\\anonymous_fn.wt").
                         name("anonymous fn")
                         .build(),
                 WattTest.builder().path(
-                                "E:\\wat_lang\\wat\\src\\test\\watt\\tests\\fp\\lambdas.wt").
+                                "G:\\Kilowatt\\Watt\\src\\test\\watt\\tests\\fp\\lambdas.wt").
                         name("lambdas")
                         .build(),
                 WattTest.builder().path(
-                                "E:\\wat_lang\\wat\\src\\test\\watt\\tests\\fp\\pipes.wt").
+                                "G:\\Kilowatt\\Watt\\src\\test\\watt\\tests\\fp\\pipes.wt").
                         name("pipes")
                         .build(),
                 WattTest.builder().path(
-                            "E:\\wat_lang\\wat\\src\\test\\watt\\tests\\reflection\\reflection.wt").
+                                "G:\\Kilowatt\\Watt\\src\\test\\watt\\tests\\reflection\\reflection.wt").
                         name("jvm reflection")
                         .build(),
                 WattTest.builder().path(
-                                "E:\\wat_lang\\wat\\src\\test\\watt\\tests\\reflection\\wattreflection.wt").
+                                "G:\\Kilowatt\\Watt\\src\\test\\watt\\tests\\reflection\\wattreflection.wt").
                         name("watt reflection")
                         .build(),
                 WattTest.builder().path(
-                            "E:\\wat_lang\\wat\\src\\test\\watt\\tests\\std\\math\\std_math_base_test.wt").
+                                "G:\\Kilowatt\\Watt\\src\\test\\watt\\tests\\std\\math\\std_math_base_test.wt").
                         name("std math base")
                         .build(),
                 WattTest.builder().path(
-                                "E:\\wat_lang\\wat\\src\\test\\watt\\tests\\std\\math\\std_math_ext_test.wt").
+                                "G:\\Kilowatt\\Watt\\src\\test\\watt\\tests\\std\\math\\std_math_ext_test.wt").
                         name("std ext base")
                         .build()
         );
