59215dcd58046e1792893d5900785176be9c2784
Slavlned <slavlendstudio@gmail.com>
2025-04-12 23:45:10 +0500

    version up

--- src/main/java/com/kilowatt/Watt.java
+++ src/main/java/com/kilowatt/Watt.java
@@ -8,7 +8,7 @@ import java.io.IOException;
  */
 public class Watt {
     // версия
-    public static final String version = "0.0.1";
+    public static final String version = "1.5.4";
 
     // экзекьютор команд
     public static final WattCommandsExecutor commandExecutor = new WattCommandsExecutor();
