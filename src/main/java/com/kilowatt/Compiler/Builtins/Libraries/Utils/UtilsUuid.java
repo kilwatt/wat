70b54526496db1118ac41342d671668db8e8d4c5
Slavlned <slavlendstudio@gmail.com>
2025-05-29 15:24:47 +0500

    updated uuid library. fixed wfiglet and crypto code formatting.

--- src/main/java/com/kilowatt/Compiler/Builtins/Libraries/Utils/UtilsUuid.java
+++ src/main/java/com/kilowatt/Compiler/Builtins/Libraries/Utils/UtilsUuid.java
@@ -1,5 +1,10 @@
 package com.kilowatt.Compiler.Builtins.Libraries.Utils;
+
 import java.util.UUID;
+
+/*
+Utils -> Uuid
+ */
 public class UtilsUuid {
     public String uuid() {
         return UUID.randomUUID().toString();
