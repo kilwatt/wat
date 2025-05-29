8d2aa5540ede298a3f669bf489676676cae14602
Sliva3 <vpfaib@gmail.com>
2025-05-29 17:18:02 +0700

    Fix and rename

--- src/main/java/com/kilowatt/Compiler/Builtins/Libraries/Utils/UtilsUuid.java
+++ src/main/java/com/kilowatt/Compiler/Builtins/Libraries/Utils/UtilsUuid.java
@@ -1,7 +1,7 @@
 package com.kilowatt.Compiler.Builtins.Libraries.Utils;
 import java.util.UUID;
 public class UtilsUuid {
-    public String getUUID() {
+    public String uuid() {
         return UUID.randomUUID().toString();
     }
 }
