480c1f4791f481c63da72e21850fcd2a81de974c
Sliva3 <vpfaib@gmail.com>
2025-05-29 17:04:38 +0700

    UUID library

---
+++ src/main/java/com/kilowatt/Compiler/Builtins/Libraries/Utils/UtilsUuid.java
@@ -0,0 +1,7 @@
+package com.kilowatt.Compiler.Builtins.Libraries.Utils;
+import java.util.UUID;
+public class UtilsUuid {
+    public String getUUID() {
+        return UUID.randomUUID().toString();
+    }
+}
